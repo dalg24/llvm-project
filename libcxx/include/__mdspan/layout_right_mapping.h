6998fdeddbdf2c8005b38891dd88f660396d196a
Christian Trott <crtrott@sandia.gov>
2023-05-10 11:02:56 -0600

    mdspan/layout: add index_operator impl for layout_right::mapping

--- libcxx/include/__mdspan/layout_right_mapping.h
+++ libcxx/include/__mdspan/layout_right_mapping.h
@@ -52,9 +52,16 @@ public:
   constexpr mapping(const extents_type& __ext) noexcept : __extents_(__ext) {}
 
   template <class _OtherExtents>
-  constexpr explicit(!is_convertible_v<_OtherExtents, extents_type>) mapping(const mapping<_OtherExtents>&) noexcept {}
+    requires(is_constructible_v<extents_type, _OtherExtents>)
+  constexpr explicit(!is_convertible_v<_OtherExtents, extents_type>)
+      mapping(const mapping<_OtherExtents>& __other) noexcept
+      : __extents_(__other.extents()) {
+    _LIBCPP_ASSERT(
+        __mdspan_detail::__is_representable_as<index_type>(__other.required_span_size()),
+        "layout_right::mapping converting ctor: other.required_span_size() not representable in index_type.");
+  }
 
-// add when we add other layouts
+// FIX_ME: add when we add other layouts
 #  if 0
     template<class _OtherExtents>
       constexpr explicit(!is_convertible_v<_OtherExtents, extents_type>)
@@ -76,9 +83,46 @@ public:
       __size *= __extents_.extent(__r);
     return __size;
   }
+
+private:
+  // Helper functions to compute the offset
+  // 3D: ((i0 * E1 + i1)*E2 + i2)*E3 + i3
+  // This is not directly doable as a fold expression, so use recursive functions here
+
+  // Helper struct to count recursion depth and when to stop
+  template <size_t _Rp, size_t _Rank>
+  struct __rank_count {};
+
+  // don't start: Rank-0
+  constexpr index_type __compute_offset(__rank_count<0, 0>) const { return 0; }
+
+  // start of recursion
+  template <class _I, class... _Indices>
+  constexpr index_type __compute_offset(__rank_count<0, extents_type::rank()>, const _I& __i, _Indices... __idx) const {
+    return __compute_offset(__i, __rank_count<1, extents_type::rank()>(), __idx...);
+  }
+
+  // continue recursion
+  template <size_t _Rp, size_t _Rank, class _I, class... _Indices>
+  constexpr index_type
+  __compute_offset(index_type __offset, __rank_count<_Rp, _Rank>, const _I& __i, _Indices... __idx) const {
+    return __compute_offset(__offset * __extents_.extent(_Rp) + __i, __rank_count<_Rp + 1, _Rank>(), __idx...);
+  }
+
+  // end of recursion
+  constexpr index_type
+  __compute_offset(index_type __offset, __rank_count<extents_type::rank(), extents_type::rank()>) const {
+    return static_cast<index_type>(__offset);
+  }
+
+public:
   template <class... _Indices>
-  constexpr index_type operator()(_Indices...) const noexcept {
-    return 0;
+    requires((sizeof...(_Indices) == extents_type::rank()) && (is_convertible_v<_Indices, index_type> && ...) &&
+             (is_nothrow_constructible_v<index_type, _Indices> && ...))
+  constexpr index_type operator()(_Indices... __idx) const noexcept {
+    _LIBCPP_ASSERT(__mdspan_detail::__is_multidimensional_index_in(__extents_, __idx...),
+                   "layout_right::mapping: out of bounds indexing");
+    return __compute_offset(__rank_count<0, extents_type::rank()>(), static_cast<index_type>(__idx)...);
   }
 
   static constexpr bool is_always_unique() noexcept { return true; }
@@ -92,8 +136,8 @@ public:
   constexpr index_type stride(rank_type) const noexcept { return 0; }
 
   template <class _OtherExtents>
-  friend constexpr bool operator==(const mapping&, const mapping<_OtherExtents>&) noexcept {
-    return false;
+  friend constexpr bool operator==(const mapping& __lhs, const mapping<_OtherExtents>& __rhs) noexcept {
+    return __lhs.extents() == __rhs.extents();
   }
 
 private:
