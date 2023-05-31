f818ac4565dcef963483b21a5991245555392dd0
Damien L-G <dalg24@gmail.com>
2023-05-31 08:58:33 -0400

    Verify that all static extents with non representable product of extents is rejected

--- libcxx/test/std/containers/views/mdspan/layout_right/extents.verify.cpp
+++ libcxx/test/std/containers/views/mdspan/layout_right/extents.verify.cpp
@@ -12,11 +12,21 @@
 // template<class Extents>
 // class layout_left::mapping;
 
-// If Extents is not a specialization of extents, then the program is ill-formed.
+// If Extents is not a specialization of extents, then the program is
+// ill-formed.
+
+// Mandates: If Extents::rank_dynamic() == 0 is true, then the size of the
+// multidimensional index space Extents() is representable as a value of type
+// typename Extents::index_type.
 
 #include <mdspan>
 
 void not_extents() {
   // expected-error-re@*:* {{{{(static_assert|static assertion)}} failed {{.*}}layout_right::mapping template argument must be a specialization of extents}}
   [[maybe_unused]] std::layout_right::mapping<void> mapping;
+}
+
+void representable() {
+  // expected-error-re@*:* {{{{(static_assert|static assertion)}} failed {{.*}}"layout_right::mapping product of static extents must be representable as index_type}}
+  [[maybe_unused]] std::layout_right::mapping<std::extents<char, 20, 20>> mapping;
 }
