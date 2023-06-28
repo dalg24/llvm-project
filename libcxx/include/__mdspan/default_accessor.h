efa8501491f0cb0753e36da2fcc731297b93dd5e
Christian Trott <crtrott@sandia.gov>
2023-06-28 11:17:00 -0600

    mdspan/default_accessor: Fix uglyfication

--- libcxx/include/__mdspan/default_accessor.h
+++ libcxx/include/__mdspan/default_accessor.h
@@ -34,22 +34,25 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if _LIBCPP_STD_VER >= 23
 
-template <class ElementType>
+template <class _ElementType>
 struct default_accessor {
-  static_assert(!is_array_v<ElementType>, "default_accessor: template argument may not be an array type");
-  static_assert(!is_abstract_v<ElementType>, "default_accessor: template argument may not be an abstract class");
+  static_assert(!is_array_v<_ElementType>, "default_accessor: template argument may not be an array type");
+  static_assert(!is_abstract_v<_ElementType>, "default_accessor: template argument may not be an abstract class");
 
   using offset_policy    = default_accessor;
-  using element_type     = ElementType;
-  using reference        = ElementType&;
-  using data_handle_type = ElementType*;
+  using element_type     = _ElementType;
+  using reference        = _ElementType&;
+  using data_handle_type = _ElementType*;
 
   _LIBCPP_HIDE_FROM_ABI constexpr default_accessor() noexcept = default;
-  template <class OtherElementType>
-    requires(is_convertible_v<OtherElementType (*)[], element_type (*)[]>)
-  _LIBCPP_HIDE_FROM_ABI constexpr default_accessor(default_accessor<OtherElementType>) noexcept {}
-  _LIBCPP_HIDE_FROM_ABI constexpr reference access(data_handle_type p, size_t i) const noexcept { return p[i]; }
-  _LIBCPP_HIDE_FROM_ABI constexpr data_handle_type offset(data_handle_type p, size_t i) const noexcept { return p + i; }
+  template <class _OtherElementType>
+    requires(is_convertible_v<_OtherElementType (*)[], element_type (*)[]>)
+  _LIBCPP_HIDE_FROM_ABI constexpr default_accessor(default_accessor<_OtherElementType>) noexcept {}
+
+  _LIBCPP_HIDE_FROM_ABI constexpr reference access(data_handle_type __p, size_t __i) const noexcept { return __p[__i]; }
+  _LIBCPP_HIDE_FROM_ABI constexpr data_handle_type offset(data_handle_type __p, size_t __i) const noexcept {
+    return __p + __i;
+  }
 };
 
 #endif // _LIBCPP_STD_VER >= 23
